fc45ec2c9fc9d254f935865d397ca451158a6d1a
Aarthi R <158204438+aarthirajavel200@users.noreply.github.com>
2025-11-19 19:12:26 +0530

    Update title to 'File Organizer Project'

--- Main/fileorganizer.py
+++ Main/fileorganizer.py
@@ -45,7 +45,7 @@ def organize_files(uploaded_files):
 
 
 st.set_page_config(page_title="File Organizer", page_icon="📁")
-st.title("📁 File Organizer")
+st.title("📁 File Organizer Project ")
 
 uploaded_files = st.file_uploader("Upload multiple files", accept_multiple_files=True)
 
